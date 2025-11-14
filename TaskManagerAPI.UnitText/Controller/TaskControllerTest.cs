d848295bea87a51eadae7e444e0bd2bd6c243777
ghaith mala osman <ghaithomo@hotmail.com>
2025-11-14 14:35:01 +0300

    #

---
+++ TaskManagerAPI.UnitText/Controller/TaskControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagerAPI.Interfaces;
+
+namespace TaskManagerAPI.UnitText.Controller
+{
+    public class TaskControllerTest
+    {
+        private readonly ITaskRepository _taskRepository;
+        public TaskControllerTest(ITaskRepository taskRepository)
+        {
+
+        }
+
+    }
+}
